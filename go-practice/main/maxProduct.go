43e12b13306fcc436835459baf875ba5c4a91e44
wangqing357 <wangqing357@jd.com>
2022-07-20 00:00:03 +0800

    feat: thinks

---
+++ go-practice/main/maxProduct.go
@@ -0,0 +1,39 @@
+package main
+
+func maxProduct(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	minRes := []int{}
+	maxRes := []int{}
+
+	minRes = append(minRes, nums[0])
+	maxRes = append(maxRes, nums[0])
+
+	res := maxRes[0]
+
+	for i := 1; i < len(nums); i++ {
+		values1 := minRes[i-1] * nums[i]
+		values2 := maxRes[i-1] * nums[i]
+		minRes = append(minRes, getMin(nums[i], getMin(values1, values2)))
+		maxRes = append(maxRes, getMax(nums[i], getMax(values1, values2)))
+		res = getMax(res, maxRes[i])
+	}
+	return res
+}
+
+func getMax(x1, x2 int) int {
+	if x1 > x2 {
+		return x1
+	} else {
+		return x2
+	}
+}
+
+func getMin(x1, x2 int) int {
+	if x1 < x2 {
+		return x1
+	} else {
+		return x2
+	}
+}
