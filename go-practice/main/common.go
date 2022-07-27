359ac2a2793ac4de361ba989a4acb74ad12ceb1a
wangqing357 <wangqing357@jd.com>
2022-07-27 23:40:36 +0800

    feat: update

---
+++ go-practice/main/common.go
@@ -0,0 +1,9 @@
+package main
+
+func getMin(x1, x2 int) int {
+	if x1 < x2 {
+		return x1
+	} else {
+		return x2
+	}
+}
--- go-practice/main/maxProduct.go
+++ go-practice/main/maxProduct.go
@@ -29,11 +29,3 @@ func getMax(x1, x2 int) int {
 		return x2
 	}
 }
-
-func getMin(x1, x2 int) int {
-	if x1 < x2 {
-		return x1
-	} else {
-		return x2
-	}
-}
